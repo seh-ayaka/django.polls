7db6ee5e4a6ad09fba39ab0ca88431cb7bd5d959
Serena <serena.tsuchiya@aluno.ifsp.edu.br>
2023-05-02 11:25:56 -0300

    criação do sobre e banco de dados

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Olá Mundo! Esta é a página iniciail do aplicativo.")
+
+def sobre(request):
+    return HttpResponse("Esta é a página sobre o meu site.")
