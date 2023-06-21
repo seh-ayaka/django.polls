11bccc18aaf8e4809b520e2363faf5967988f6d8
serena <serena.tsuchiya@aluno.ifsp.edu.br>
2023-06-21 08:16:16 -0300

    alterar views

--- polls/views.py
+++ polls/views.py
@@ -6,9 +6,6 @@ def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     output = ", ".join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
-    
-def index(request):
-    return HttpResponse("Página Inicial.")
 
 def detail(request, question_id):
     return HttpResponse(f"Essa é pergunta de número {question_id}.")
