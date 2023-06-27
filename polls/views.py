83f3496e1296f8c84beb6f0b4d1bf3d6ae7d54b7
Serena <serena.tsuchiya@aluno.ifsp.edu.br>
2023-06-27 09:16:27 -0300

    404 error

--- polls/views.py
+++ polls/views.py
@@ -17,3 +17,11 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse(f"Você vai votar na pergunta de número {question_id}")
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
