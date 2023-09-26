b264a1f72650e279b1d0b400448fffca6426344f
serena <serena.tsuchiya@aluno.ifsp.edu.br>
2023-09-26 10:31:33 -0300

    add pasta account, forms: import forms/user

--- my_site/settings.py
+++ my_site/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig', #pode deixar só como 'account' tbm mas este é o completo
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = '/admin'
+LOGOUT_REDIRECT_URL = '/admin'
---
+++ polls/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+#importar o model "User" padrão para usuários
+from django.contrib.auth import get_user_model
